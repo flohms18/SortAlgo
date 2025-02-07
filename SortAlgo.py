653bfd5255732d6753f42abb2c43952cbe82516c
flotr18 <trouilletflorent@outlook.com>
2025-02-07 11:36:30 +0100

    Added Bubble Algo

---
+++ SortAlgo.py
@@ -0,0 +1,20 @@
+import random
+
+MyArray = []
+
+for i in range(10):
+    MyArray.append(random.randint(1,100))
+print(MyArray)
+
+def BubbleArray():
+    for i in range(len(MyArray)):
+        swapped = False
+        for j in range(len(MyArray) - i - 1):
+            if MyArray[j] > MyArray[j + 1]:
+                MyArray[j], MyArray[j + 1] = MyArray[j + 1], MyArray[j]
+                swapped = True
+        if not swapped:
+                break
+    return MyArray
+
+print(BubbleArray())
